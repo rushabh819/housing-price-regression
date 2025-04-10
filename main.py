09b459f707a96f24d84903ac0427e8ce03f6d01c
Rush <rparmar.rp198@gmail.com>
2025-04-10 18:33:42 -0400

    .

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def main():
     dw.run_data_wrangling(raw_path= cleaned_path, cleaned_path= cleaned_path)
     sf.runScaling_features(origin_path= cleaned_path, dest_path= cleaned_path)
 
-    data = pd.read_csv(cleaned_path)
-    print(data.head())
+    # data = pd.read_csv(cleaned_path)
+    # print(data.head())
 main()
